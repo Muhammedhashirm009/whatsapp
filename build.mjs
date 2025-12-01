457f9eacf17c74dbab86733f257255110c39ebb3
Muhammedhashirm009 <87589712+Muhammedhashirm009@users.noreply.github.com>
2025-12-01 22:54:43 +0530

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -2,12 +2,12 @@
 import { execSync } from 'child_process';
 
 const esmShim = `
-import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const require = createRequire(import.meta.url);
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { createRequire as _createRequire } from 'module';
+import { fileURLToPath as _fileURLToPath } from 'url';
+import { dirname as _dirname } from 'path';
+const require = _createRequire(import.meta.url);
+const __filename = _fileURLToPath(import.meta.url);
+const __dirname = _dirname(__filename);
 `;
 
 try {
